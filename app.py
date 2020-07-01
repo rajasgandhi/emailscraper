f8e8d744aaa35d2251f548904a151cf978c2f8cc
rajasgandhi <rajasmgandhi@gmail.com>
2020-07-01 12:24:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -83,9 +83,10 @@ def fetch(url, session):
     return re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", r.html.html)'''
     firefox_options=webdriver.FirefoxOptions()
     firefox_options.binary_location = os.environ.get('FIREFOX_BIN')
-    firefox_options.add_argument("--headless")
-    firefox_options.add_argument("--disable-dev-shm-usage")
-    firefox_options.add_argument("--no-sandbox")
+    firefox_options.headless = True
+    #firefox_options.add_argument("--headless")
+    #firefox_options.add_argument("--disable-dev-shm-usage")
+    #firefox_options.add_argument("--no-sandbox")
     driver = webdriver.Firefox(executable_path=os.environ.get('GECKODRIVER_PATH'), firefox_options=firefox_options)
     driver.get(url)
     html=driver.execute_script("return document.documentElement.outerHTML")
